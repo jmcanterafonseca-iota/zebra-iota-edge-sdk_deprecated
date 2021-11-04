778b1a061541bef2fa0c346f0b2b7449662e37c5
Severin Skillman <severin.skillman@iota.org>
2021-11-04 12:10:42 +0100

    updated iota identity

--- Identity.md
+++ Identity.md
@@ -5,15 +5,10 @@
 import { ServiceFactory } from '../factories/serviceFactory';
 import { IdentityService } from '../services/identityService';
 import { IOTA_NODE_URL, DEVNET } from '../config';
-import type { IdentityConfig } from '../models/types/identity';    
 
-const config: IdentityConfig = {
-	  node: IOTA_NODE_URL,
-	  network: DEVNET ? 'dev' : 'main',
-};
 
 export default () => {
-      ServiceFactory.register('identity', () => new IdentityService(config));  
+      ServiceFactory.register('identity', () => new IdentityService(DEVNET, IOTA_NODE_URL));  
 }
 ```
 
--- identity-enabler/holder-mobile-app/src/models/types/identity.ts
+++ identity-enabler/holder-mobile-app/src/models/types/identity.ts
@@ -1,18 +1,13 @@
-import type * as IotaIdentity from '@iota/identity-wasm/web';
+import type { Document, KeyPair, KeyCollection, VerificationMethod } from '@iota/identity-wasm/web';
 
 export type Identity = {
   didDoc: string;
   publicAuthKey: string;
   privateAuthKey: string;
-  doc: typeof IotaIdentity.Document;
-  key: typeof IotaIdentity.KeyPair;
-  keys: typeof IotaIdentity.KeyCollection | any;
-  method: typeof IotaIdentity.VerificationMethod | any;
-};
-
-export type IdentityConfig = {
-  node: string;
-  network: string;
+  doc: Document;
+  key: KeyPair;
+  keys: KeyCollection | any;
+  method: VerificationMethod | any;
 };
 
 export type VerifiableCredentialEnrichment = {
