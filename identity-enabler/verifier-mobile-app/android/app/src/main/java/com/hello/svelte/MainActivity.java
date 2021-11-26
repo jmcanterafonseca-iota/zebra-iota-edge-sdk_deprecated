a0bb1b450a4fb2996bdab3b8acce401506e43393
Severin Skillman <severin.skillman@iota.org>
2021-11-26 17:32:33 +0100

    android app renaming

--- identity-enabler/verifier-mobile-app/android/app/src/main/java/com/hello/svelte/MainActivity.java
+++ identity-enabler/verifier-mobile-app/android/app/src/main/java/com/hello/svelte/MainActivity.java
@@ -1,23 +1,27 @@
-package org.iota.zebra.device;
+package org.iota.zebra.verifier;
 
 import android.os.Bundle;
+import android.content.pm.ActivityInfo;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 import com.ahm.capacitor.camera.preview.CameraPreview;
+import com.dutchconcepts.capacitor.barcodescanner.BarcodeScanner;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
+    this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
       add(CameraPreview.class);
+      add(BarcodeScanner.class);
     }});
   }
 }
