8f5933366ea159ae660456c1a70340f21f57522e
Severin Skillman <severin.skillman@iota.org>
2021-12-17 17:31:09 +0100

    added back button functionality to new landing page

--- identity-enabler/verifier-mobile-app/src/pages/Landing.svelte
+++ identity-enabler/verifier-mobile-app/src/pages/Landing.svelte
@@ -1,12 +1,38 @@
 <script lang="ts">
+    import { Plugins } from "@capacitor/core";
+    import { onMount } from "svelte";
     import { navigate } from "svelte-routing";
     import Button from "../components/Button.svelte";
     import Header from "../components/Header.svelte";
+    import { BACK_BUTTON_EXIT_GRACE_PERIOD } from "../config";
+    import { wait } from "../lib/helpers";
     import { firstLaunch } from "../lib/store";
+
+    const { App, Toast } = Plugins;
+    let exitOnBack = false;
+
+    onMount(() => App.addListener("backButton", onBack).remove);
 
     function onNext() {
         firstLaunch.set(false);
         navigate("/home");
+    }
+
+    async function onBack() {
+        if (exitOnBack) {
+            // From the landing screen, navigating back twice should exit the app
+            App.exitApp();
+            return;
+        }
+
+        exitOnBack = true;
+        await Toast.show({
+            position: "bottom",
+            duration: "short",
+            text: "Tap back again to exit"
+        });
+        await wait(BACK_BUTTON_EXIT_GRACE_PERIOD);
+        exitOnBack = false;
     }
 </script>
 
